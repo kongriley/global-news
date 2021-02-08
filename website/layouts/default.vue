c314e63630bd4c68f5b6fdbd014dfee113d2ee80
Riley Kong <kongriley@gmail.com>
2021-02-07 17:56:30 -0800

    Initial nuxt app

---
+++ website/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family:
+    'Inter',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+}
+</style>
